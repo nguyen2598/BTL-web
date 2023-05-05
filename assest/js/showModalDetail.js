28126f4414fbd1d5a9baaf2cfe9c154d2046f9c7
dinhnguyen2598 <dinhnguyen250802@gmail.com>
2023-05-05 09:52:31 +0700

    update product-detail

--- assest/js/showModalDetail.js
+++ assest/js/showModalDetail.js
@@ -9,6 +9,22 @@ const imgDetails = document.querySelectorAll('.home-product--img img');
 imgDetails.forEach((imgDetail, index) => {
     imgDetail.addEventListener('click', () => {
         modalDetail.classList.remove('hidden');
+        console.log(modalDetail);
+        console.log(imgDetail.parentElement.parentElement);
+        const parentItemDetail = imgDetail.parentElement.parentElement;
+        const imageDetail = parentItemDetail.querySelector('img');
+        console.log(imageDetail);
+        const nameDetail = parentItemDetail.querySelector('.home-product--title');
+        const oldDetail = parentItemDetail.querySelector('.home-product__old');
+        const saleDetail = parentItemDetail.querySelector('.home-product__sale');
+        //
+
+        modalDetail.querySelector('.modal-body .modal-body--content__header').innerText = nameDetail.innerText;
+        modalDetail.querySelector('.modal-body .modal-body--slide-show .modal-body--slide-show--1').src =
+            imageDetail.src;
+        modalDetail.querySelector('.modal-body .modal-body--price__old').innerText = oldDetail.innerText;
+        modalDetail.querySelector('.modal-body .modal-body--price__sale').innerText = saleDetail.innerText;
+        // modalDetail.querySelector('.modal-body .modal-percent--sale').innerText=
     });
 });
 
